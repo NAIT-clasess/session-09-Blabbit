449bed9bd5cc96088a033f6dc752bbd0c1e05e46
Blabbit <tjhbumbry@gmail.com>
2026-02-03 14:43:43 -0700

    Changed for pong game

--- Game1.cs
+++ Game1.cs
@@ -9,6 +9,15 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
+    private const int _preferredScreenWidth = 750, _preferredScreenHeight = 450;
+    private const int _playAreaEdgeLineWidth = 12, _ballWidthAndHeight = 50;
+
+    private float _ballSpeed = 10;
+    private Vector2 _ballPosition,_ballDirection;
+
+    private Texture2D _backgroundTexture, _ballTexture; 
+
+    private Rectangle _playAreaBoundingBox;
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -18,16 +27,27 @@ public class Game1 : Game
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
+        _graphics.PreferredBackBufferWidth = _preferredScreenWidth;
+        _graphics.PreferredBackBufferHeight = _preferredScreenHeight;
 
+        _ballPosition.X = 150;
+        _ballPosition.Y = 195;
+        _ballSpeed = 200;
+
+        _ballDirection.X = -1;
+        _ballDirection.Y = -1;
+
+        _playAreaBoundingBox = new(0, 0, _preferredScreenWidth, _preferredScreenHeight);
+        _graphics.ApplyChanges();
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        // TODO: use this.Content to load your game content here
+        _backgroundTexture = Content.Load<Texture2D>("Court");
+        _ballTexture = Content.Load<Texture2D>("Ball");
+        
     }
 
     protected override void Update(GameTime gameTime)
@@ -35,7 +55,21 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
+        _ballPosition += _ballDirection * _ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        // Ball collision with left/right edges
+        if (_ballPosition.X <= _playAreaBoundingBox.Left ||
+            _ballPosition.X + _ballWidthAndHeight >= _playAreaBoundingBox.Right)
+        {
+            _ballDirection.X *= -1;
+        }
+
+        // Ball collision with top/bottom edges
+        if (_ballPosition.Y <= _playAreaBoundingBox.Top ||
+            _ballPosition.Y + _ballWidthAndHeight >= _playAreaBoundingBox.Bottom)
+        {
+            _ballDirection.Y *= -1;
+        }
 
         base.Update(gameTime);
     }
@@ -44,8 +78,15 @@ public class Game1 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(_backgroundTexture, _playAreaBoundingBox, Color.White);
 
-        base.Draw(gameTime);
+        var ballRect = new Rectangle((int)_ballPosition.X, (int)_ballPosition.Y, _ballWidthAndHeight, _ballWidthAndHeight);
+
+        _spriteBatch.Draw(_ballTexture, ballRect, Color.White);
+
+        _spriteBatch.End();
+
+        base.Draw(gameTime); 
     }
 }
